83d638a06c18ea11051e50494166821639697ed7
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2013-07-21 01:44:54 -0400

    Created a class which will store the history of a device. The root locations is has had, and the last known hashes of various subdirectories.

---
+++ test/falgout/backup/app/HashTest.java
@@ -0,0 +1,22 @@
+package falgout.backup.app;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class HashTest {
+	@Test
+	public void HashIsImmutable() {
+		byte[] b = { 1, 2, 3, 4, 5 };
+		byte[] b2 = Arrays.copyOf(b, 5);
+		Hash h = new Hash(b2);
+		byte[] h2 = h.getHash();
+		b2[0] = 9;
+		assertArrayEquals(b, h2);
+		h2[0] = 9;
+		byte[] h3 = h.getHash();
+		assertArrayEquals(b, h3);
+	}
+}
