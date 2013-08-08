dac7ea77b765c1a2c4a61bed87513accd5d57dfb
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2013-08-08 15:49:43 -0400

    Expanded the Manager interface to cover restoration of files to FileStores/arbitrary locations and enumeration of the managed devices

--- src/falgout/backup/app/AbstractManager.java
+++ src/falgout/backup/app/AbstractManager.java
@@ -2,14 +2,17 @@ package falgout.backup.app;
 
 import java.io.IOException;
 import java.nio.file.FileStore;
-
-import com.google.inject.Inject;
+import java.nio.file.Path;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public abstract class AbstractManager implements Manager {
     private final DeviceFactory factory;
     
-    @Inject
-    public AbstractManager(DeviceFactory factory) {
+    protected AbstractManager(DeviceFactory factory) {
         this.factory = factory;
     }
     
@@ -23,4 +26,36 @@ public abstract class AbstractManager implements Manager {
     }
     
     protected abstract void doBackup(Device dev) throws IOException;
+    
+    @Override
+    public void restore(FileStore store) throws IOException {
+        restore(store, Calendar.getInstance());
+    }
+    
+    @Override
+    public void restore(FileStore store, Calendar date) throws IOException {
+        Device dev = factory.create(store);
+        doRestore(dev, dev.getRoot(), date);
+    }
+    
+    @Override
+    public void restore(UUID id, Path dir) throws IOException {
+        restore(id, dir, Calendar.getInstance());
+    }
+    
+    @Override
+    public void restore(UUID id, Path dir, Calendar date) throws IOException {
+        doRestore(factory.create(id), dir, date);
+    }
+    
+    protected abstract void doRestore(DeviceData data, Path dir, Calendar date) throws IOException;
+    
+    @Override
+    public Map<UUID, Set<Calendar>> getBackupDates() throws IOException {
+        Map<UUID, Set<Calendar>> dates = new LinkedHashMap<>();
+        for (UUID id : getManagedDevices()) {
+            dates.put(id, getBackupDates(id));
+        }
+        return dates;
+    }
 }
--- src/falgout/backup/app/Manager.java
+++ src/falgout/backup/app/Manager.java
@@ -2,7 +2,26 @@ package falgout.backup.app;
 
 import java.io.IOException;
 import java.nio.file.FileStore;
+import java.nio.file.Path;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public interface Manager {
     public void backup(FileStore store) throws IOException;
+    
+    public void restore(FileStore store) throws IOException;
+    
+    public void restore(FileStore store, Calendar date) throws IOException;
+    
+    public void restore(UUID id, Path dir) throws IOException;
+    
+    public void restore(UUID id, Path dir, Calendar date) throws IOException;
+    
+    public Set<UUID> getManagedDevices() throws IOException;
+    
+    public Set<Calendar> getBackupDates(UUID id) throws IOException;
+    
+    public Map<UUID, Set<Calendar>> getBackupDates() throws IOException;
 }
