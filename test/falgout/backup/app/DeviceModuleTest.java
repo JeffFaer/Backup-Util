808574d041be97c1caa0eea4b62460985c2de3b2
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2013-08-08 10:03:14 -0400

    Ensured that the FileStore passed through the generated DeviceFactory goes through to the Device, even if another FileStore was bound by a module.

--- test/falgout/backup/app/DeviceModuleTest.java
+++ test/falgout/backup/app/DeviceModuleTest.java
@@ -1,10 +1,12 @@
 package falgout.backup.app;
 
 import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.nio.file.FileStore;
 import java.nio.file.FileSystems;
+import java.nio.file.Paths;
 
 import org.jukito.JukitoRunner;
 import org.junit.Rule;
@@ -16,6 +18,7 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 
 import falgout.backup.BackupModule;
+import falgout.backup.MockModule;
 import falgout.backup.TemporaryFileStore;
 
 @RunWith(JukitoRunner.class)
@@ -27,7 +30,9 @@ public class DeviceModuleTest {
     @Test
     public void CreatesDeviceWithProvidedStore() throws IOException {
         FileStore store = FileSystems.getDefault().getFileStores().iterator().next();
-        Injector i = Guice.createInjector(backup, device);
+        when(fs.locator.getRootLocation(store)).thenReturn(Paths.get(""));
+        
+        Injector i = Guice.createInjector(backup, device, new MockModule(fs));
         DeviceFactory f = i.getInstance(DeviceFactory.class);
         Device d = f.create(store);
         
