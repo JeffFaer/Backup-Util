4238990e96f1b59f21fb4ac74bf1ef3c21d85444
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2013-09-11 19:17:33 -0400

    Changed DeviceModule to take an MessageDigest as an optional parameter.

--- src/falgout/backup/app/DeviceModule.java
+++ src/falgout/backup/app/DeviceModule.java
@@ -6,11 +6,25 @@ import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 
 public class DeviceModule extends AbstractModule {
+    private final MessageDigest md;
+    
+    public DeviceModule() {
+        this(null);
+    }
+    
+    public DeviceModule(MessageDigest md) {
+        this.md = md;
+    }
+    
     @Override
     protected void configure() {
         install(new FactoryModuleBuilder().implement(Device.class, DefaultDevice.class)
                 .implement(DeviceData.class, DefaultDevice.Data.class)
                 .build(DeviceFactory.class));
-        requireBinding(MessageDigest.class);
+        if (md != null) {
+            bind(MessageDigest.class).toInstance(md);
+        } else {
+            requireBinding(MessageDigest.class);
+        }
     }
 }
